f38f62feb286bd00929c76887fb67d007274fcaf
sla055677 <sla055677@gmail.com>
2020-11-01 22:12:20 +0200

    initial code +Welcome0254

---
+++ dotNet5781_00_8458_0254/dotNet5781_00_8458_0254/Program0254.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_8458_0254
+{
+   partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome0254();
+            Welcome8458();
+            Console.ReadKey();
+        }
+        static partial void Welcome8458();
+        private static void Welcome0254()
+
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = System.Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
