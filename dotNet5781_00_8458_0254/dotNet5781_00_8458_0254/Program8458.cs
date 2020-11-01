69858901d4a6e403bc8b4083f576f15dfe37b83e
Chani Zilbershlag <hnyzylbrslgg@gmail.com>
2020-11-01 22:31:20 +0200

    change1

---
+++ dotNet5781_00_8458_0254/dotNet5781_00_8458_0254/Program8458.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_8458_0254
+{
+    partial class Program
+    {
+        static partial void Welcome8458()
+        {
+            Console.WriteLine("I am also here");
+        }
+    }
+}
